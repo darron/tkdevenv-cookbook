79c136a9164bab7157def99d29bdb0ae5b48fad5
Darron Froese <darron@froese.org>
2014-05-05 08:53:17 -0600

    Adding ChefSpec and Serverspec tests.

--- spec/default_spec.rb
+++ spec/default_spec.rb
@@ -5,7 +5,31 @@ require 'spec_helper'
 describe 'tkdevenv::default' do
   let(:chef_run) { ChefSpec::Runner.new.converge(described_recipe) }
 
-  it 'does something' do
-    pending 'Replace this with meaningful tests'
+  it 'includes the `ruby2` recipe' do
+    expect(chef_run).to include_recipe('ruby2::default')
+  end
+
+  it 'includes the `ubuntu_base` recipe' do
+    expect(chef_run).to include_recipe('ubuntu_base::default')
+  end
+
+  it 'includes the `chef-sugar` recipe' do
+    expect(chef_run).to include_recipe('chef-sugar::default')
+  end
+
+  it 'includes the `docker` recipe' do
+    expect(chef_run).to include_recipe('docker::default')
+  end
+
+  it 'installs libxml2-dev' do
+    expect(chef_run).to install_package('libxml2-dev')
+  end
+
+  it 'installs libxslt1-dev' do
+    expect(chef_run).to install_package('libxslt1-dev')
+  end
+
+  it 'installs wget' do
+    expect(chef_run).to install_package('wget')
   end
 end
--- test/integration/default/serverspec/default_spec.rb
+++ test/integration/default/serverspec/default_spec.rb
@@ -3,7 +3,32 @@ require 'spec_helper'
 
 # Write integration tests with Serverspec - http://serverspec.org/
 describe 'tkdevenv::default' do
-  it 'does something' do
-    pending 'Replace this with meaningful tests'
+  describe package('lxc-docker') do
+    it { should be_installed }
+  end
+
+  describe service('docker') do
+    it { should be_enabled }
+    it { should be_running }
+  end
+
+  describe port(4243) do
+    it { should be_listening }
+  end
+
+  describe package('ruby2.0') do
+    it { should be_installed }
+  end
+
+  describe package('ruby2.0-dev') do
+    it { should be_installed }
+  end
+
+  describe package('virtualbox-4.3') do
+    it { should be_installed }
+  end
+
+  describe package('vagrant') do
+    it { should be_installed }
   end
 end
